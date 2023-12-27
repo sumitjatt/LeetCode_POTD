f23e9f3fc832dd6c00ed55e5de3a4c179713ecf4
sumitjatt <137871625+sumitjatt@users.noreply.github.com>
2023-12-27 22:12:46 +0530

    Add files via upload

---
+++ 10leetcode.cpp
@@ -0,0 +1,24 @@
+//1578. Minimum Time to Make Rope Colorful
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int ans = 0;
+        for(int i = 0 ; i < colors.size()-1 ; i++){
+            if(colors[i] == colors[i+1]){
+                if(neededTime[i]>neededTime[i+1]){
+                    ans += neededTime[i+1];
+                    neededTime[i+1] = neededTime[i];
+                }
+                else{
+                    ans += neededTime[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
