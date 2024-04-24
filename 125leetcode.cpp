58a16dfbe2856b7a31613813f7beea2794e94ece
sumitjatt <137871625+sumitjatt@users.noreply.github.com>
2024-04-24 11:32:48 +0530

    Add files via upload

---
+++ 125leetcode.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // ITREATIVE SOLUTION
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int a = 0 , b = 1 , c= 1;
+        for(int i = 3 ; i <= n ; i++){
+            int temp = a+b+c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+        return c;
+    }
+};
