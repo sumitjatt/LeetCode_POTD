9fc5894fab1c7fd07023e3dbd3811b4f074d06e2
sumitjatt <137871625+sumitjatt@users.noreply.github.com>
2024-06-13 13:01:56 +0530

    Add files via upload

---
+++ 161leetcode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+        int ans =0;
+        for(int i = 0 ; i < seats.size() ; i++){
+            int a = abs(seats[i]-students[i]);
+            ans += a;
+        }
+        return ans;
+    }
+};
