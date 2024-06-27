c25c77779bc026ad1a46f4ddcfc207a4b1b4c9ed
sumitjatt <137871625+sumitjatt@users.noreply.github.com>
2024-06-27 12:58:17 +0530

    Add files via upload

---
+++ 172leetcode.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        bitset<100000> visited=0;
+        for(auto& e: edges){
+            int v=e[0], w=e[1];
+            if (visited[v]) return v;
+            if (visited[w]) return w;
+            visited[v]=visited[w]=1;
+        }
+        return -1;
+    }
+};
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
